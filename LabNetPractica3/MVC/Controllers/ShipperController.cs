360553086b85b801d022be3e7545a68a43540c96
SosaRaul <tecraulsosa@gmail.com>
2023-09-12 10:47:07 -0300

    MVC insert

---
+++ LabNetPractica3/MVC/Controllers/ShipperController.cs
@@ -0,0 +1,58 @@
+﻿using LabNetPractica3.Entities;
+using LabNetPractica3.Logic;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    
+    public class ShipperController : Controller
+    {
+        ShippersLogic shippersLogic = new ShippersLogic();
+        public ActionResult Index()
+        {
+            
+            List<Shippers> shippers = shippersLogic.GetAll();
+            
+            List<ShipperView> shippersViews = shippers.Select(shipper => new ShipperView
+            {
+                Phone = shipper.Phone,
+                CompanyName = shipper.CompanyName
+            }).ToList();
+            
+            return View(shippersViews);
+        }
+        [HttpPost]
+        public ActionResult Insert(ShipperView shipperView)
+        {
+            try
+            {
+                Shippers shipper = new Shippers {
+                    CompanyName = shipperView.CompanyName,
+                    Phone = shipperView.Phone,
+                };
+                shippersLogic.Add(shipper);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+   /*     public ActionResult Update() 
+        { 
+        
+        }
+
+        public ActionResult Delete() 
+        { 
+        }
+   */
+    }
+}
---
+++ LabNetPractica3/MVC/Models/ShipperView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class ShipperView
+    {
+        public string CompanyName{ get; set;}
+        public string Phone { get; set;}    
+
+        
+    }
+}
